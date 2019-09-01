fbc46f40c7719607a4eaa82f51f99f2b98bb235a
pablo-psichalis <pablo.psrz@gmail.com>
2019-09-02 00:15:56 +0200

    Messing with examples from the docs

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const THREE = require('three');
-
+import Detector from './src/utils/webgl-detector';
 import './styles/style.css';
 
-// Very basic spinning cube example
-import './src/cube';
+// Examples
+const example01 = require('./src/examples-docs/cube');
+const example02 = require('./src/examples-docs/line-drawing');
 
+function init() {
+    // Check for webGL capabilities
+    if (!Detector.webgl) {
+        Detector.addGetWebGLMessage();
+    }
+
+    // Load example scripts
+    // example01.render();
+    example02.render();
+}
+
+init();
